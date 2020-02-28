a494209e0287dda4e8e204280fbedf0b5ce0442c
tom-englert <mail@tom-englert.de>
2020-02-28 17:44:40 +0100

    Add resources to state

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,12 +5,15 @@ import {
   on
 } from '@ngrx/store';
 import {environment} from '../../environments/environment';
-import {loadL10N, loadL10NSuccess} from '../actions/l10n.actions';
+import {loadL10N, loadL10NReset, loadL10NSuccess} from '../actions/l10n.actions';
+import {Resources} from '../resources';
 
 export interface L10nState {
+  resources: Resources;
 }
 
 const initialL10NState: L10nState = {
+  resources: new Resources()
 };
 
 export interface State {
@@ -25,7 +28,11 @@ export const l10nReducer = createReducer(
   }),
   on(loadL10NSuccess, (state, {data}) => {
     console.log('l10nReducer:', 'success', state, data);
-    return state;
+    return {...state, resources: data.Resources};
+  }),
+  on(loadL10NReset, (state, {}) => {
+    console.log('l10nReducer:', 'reset', state);
+    return {...state, resources: new Resources()};
   })
 );
 
